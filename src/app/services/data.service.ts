a5cef9a0093e46c0adef163dca1d67a7a631ae29
volatildub <nicolas.hardmeier@yahoo.es>
2018-06-29 13:11:53 +0200

    He quitado de data service unos modulos y variables que no se utilizaban

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class DataService {
-  private dish = new BehaviorSubject<any>([]);
-  currentDish = this.dish.asObservable();
 
   constructor(private http: HttpClient) { }
 
